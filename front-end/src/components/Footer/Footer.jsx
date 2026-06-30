65249d8916478073d13f387be5e42ddf8b24b81e
togbolo <87999565+Togbolo@users.noreply.github.com>
2026-06-30 21:13:26 +0000

    Add files via upload

---
+++ front-end/src/components/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt=""  />
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pulvinar, neque vel scelerisque accumsan, est erat imperdiet nunc, vel iaculis urna justo nec nunc. Suspendisse euismod a dui a fringilla. Aenean sed magna facilisis, varius justo eu, commodo magna. Maecenas non ante nibh. Integer sollicitudin magna nec est vestibulum, consequat malesuada sem suscipit. </p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Delivery</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+1-212-456-7890</li>
+                    <li>contact@tomato.com</li>
+                </ul>
+            </div>
+            
+        </div>
+        <hr />
+        <p className='footer-copyright'>
+            Copyright 2025 &copy; Tomato.com All- Right Reserved.
+        </p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ front-end/src/components/header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+              Sed pulvinar, neque vel scelerisque accumsan, est erat imperdiet nunc, 
+              vel iaculis urna justo nec nunc. Suspendisse euismod a dui a fringilla. 
+              Aenean sed magna facilisis, varius justo eu, commodo magna. 
+              Maecenas non ante nibh. Integer sollicitudin magna nec est vestibulum,
+               consequat malesuada sem suscipit. </p>
+            <button>View Menu</button>
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
